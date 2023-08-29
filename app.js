d80b1e9390740097756238e1755f633678347f8d
ichal321 <ichal321@gmail.com>
2023-08-29 10:31:48 +0700

    update file appjs

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/app2', (req, res) => {
   res.send('Hello this App 2!')
 });
 
+app.get('/fahrizal', (req, res) => {
+  res.send('Belajar DevOps IDN')
+});
+
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
